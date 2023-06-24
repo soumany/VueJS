463b1ea85f357ac9dfefb3bbff2c4461ea52f0c6
soumany <soumany06@gmail.com>
2023-06-24 14:18:37 +0700

    #4 Data Binding

--- app.js
+++ app.js
@@ -2,7 +2,9 @@ new Vue({
     el:'#vue-app',
     data: {
         name: 'Shaun',
-        job: 'Ninja'
+        job: 'Ninja',
+        website: 'https://www.google.com/',
+        websiteTag: '<a href= "https: //www.google.com"> The google website </a>'
     },
     methods: {
         greet: function(time){
