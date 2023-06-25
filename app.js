e3f524e9852ceaadbf2eb4f4ccd0c7aff20f0947
soumany <soumany06@gmail.com>
2023-06-25 17:35:19 +0700

    #5 Event

--- app.js
+++ app.js
@@ -3,12 +3,19 @@ new Vue({
     data: {
         name: 'Shaun',
         job: 'Ninja',
+        age: 20,
         website: 'https://www.google.com/',
         websiteTag: '<a href= "https: //www.google.com"> The google website </a>'
     },
     methods: {
         greet: function(time){
             return 'Good' + time + '' + this.name;
+        },
+        add: function(inc){
+            this.age+=inc;
+        },
+        subtract: function(dec){
+            this.age-=dec;
         }
     }
 });
