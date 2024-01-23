63fd389c86cee8d9067a86440c45adaba249a956
soumany <soumany06@gmail.com>
2024-01-23 14:24:04 +0700

    handle a form submission

--- Vue_laracast/Ep6/js/components/Assignments.js
+++ Vue_laracast/Ep6/js/components/Assignments.js
@@ -6,6 +6,14 @@ export default {
         <section class="space-y-6">
             <assignment-list :assignments="filters.inProgress" title="In Progress"></assignment-list>
             <assignment-list :assignments="filters.completed" title="Complete"></assignment-list>
+
+            <form @submit.prevent="add">
+                <div class="border border-gray-600 text-black">
+                    <input v-model="newAssignment" placeholder="New Assignment..." class="p-2" />
+                    <button type= "submit" class="bg-white p-2 border-l">Add</button>
+                </div>
+                
+            </form>
         </section>
 
         
@@ -18,7 +26,9 @@ export default {
                 { name: 'Finish Project', complete: false, id: 1},
                 { name: 'Read chapter 4', complete: false, id: 2},
                 { name: 'Turn in Homework', complete: false, id: 3},
-            ]
+            ],
+
+            newAssignment: ''
         }
     },
 
@@ -29,6 +39,17 @@ export default {
                 inProgress: this.assignments.filter(assignment => !assignment.complete),
                 completed: this.assignments.filter(assignment => assignment.complete),
             }
+        }
+    },
+    methods: {
+        add() {
+            this.assignments.push({
+                name: this.newAssignment,
+                completed: false,
+                id: this.assignments.length + 1
+            });
+
+            this.newAssignment = '';
         }
     }
 
