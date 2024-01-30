e864840d5f018b1bbb47c9bf083316294e7b0b42
soumany <soumany06@gmail.com>
2024-01-30 15:21:54 +0700

    commit

--- Vue_laracast/Ep6/js/components/AssignmentList.js
+++ Vue_laracast/Ep6/js/components/AssignmentList.js
@@ -1,13 +1,14 @@
 import Assignment from "./Assignment.js"
+import AssignmentTags from "./AssignmentTags.js";
 export default {
-    components: { Assignment},
+    components: { Assignment, AssignmentTags},
     template : `
         <section v-show="assignments?.length">
             <h2 class="font-bold mb-2">{{ title }}
                 <span>({{assignments.length}})</span>
             </h2>
 
-            
+            <assignment-tags :initial-tags="assignment.map(a => a.tag)" />
 
             <ul class="border border-gray-600 divide-y divide-gray-600 mt-6">
                 <assignment 
@@ -37,9 +38,5 @@ export default {
             }
             return this.assignments.filter(a => a.tag === this.currentTag);
         },
-
-        tags() {
-            return ['all', ...new Set(this.assignments.map(a => a.tag))];
-        }
     }
 }
